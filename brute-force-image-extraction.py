58b34d85b6aa8e5ab319732a5d2e9381333cad37
vigil-senpai <vincent.pradipta@binus.ac.id>
2022-05-30 23:25:51 +0700

    file and wordlist used

---
+++ brute-force-image-extraction.py
@@ -0,0 +1,36 @@
+from threading import Thread, Lock, current_thread
+from queue import Queue
+import time
+
+def load_word_list(wordlist_filename):
+    print('[*] Loading wordlist')
+    with open(wordlist_filename) as wordlist_file:
+        for word in wordlist_file:
+            wordlist.put(word[:-1])
+            pass
+
+def brute(wordlist, lock):
+    while not wordlist.empty(): 
+        word = wordlist.get()
+        with lock:
+            print(current_thread().name, word)
+        wordlist.task_done()
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+    wordlist = Queue()
+    load_word_list('rockyou.txt') # you may change this value
+    threads_num = 100 # you may change this value
+    lock = Lock()
+    
+    for x in range(threads_num): 
+        thread = Thread(name=str(x + 1), target=brute, args=(wordlist, lock))
+        thread.daemon = True # dies when the main thread dies
+        thread.start()
+
+    wordlist.join()
+
+    finished_time = time.time()
+    print('[*] END')
+    print('[*] Time needed {}'.format(finished_time - start_time))
